678048fb216b8adfe49c35f9b0c2da7a9b68bcf9
Rudhrakumar <rudhrakumar@gmail.com>
2014-09-01 11:35:39 +1000

    updated documents

---
+++ Group Submission/JPRS Squad Group Submission_Construction 1/Programming/Completed Programs/PHP_PRES/conferences/edit.php
@@ -0,0 +1,399 @@
+<div id='conference_edit'>
+<link rel='stylesheet' type='text/css' href='CSS/std_form.css'>
+<?PHP
+/*
+ _____ _____ _____ _____    _____ _____ _____ _____ _____ _____
+|_   _|   __|  _  |     |  |   __|  _  |  _  | __  |_   _|  _  |
+  | | |   __|     | | | |  |__   |   __|     |    -| | | |     |
+  |_| |_____|__|__|_|_|_|  |_____|__|  |__|__|__|__| |_| |__|__|
+
+ * File: edit.php
+ * Edit Presenter for the dstc e conference web application.
+ * Written by TEAM SPARTA
+ * Last updated: 25-03-14 by Andy
+*/
+
+////
+//// Setup START
+////
+
+// Import libraries.
+require "PHP_PRES/helpers/dateTimePicker.php";
+require "PHP_DB/dbObject.php";
+
+$validated = false;
+global $userid;
+
+// Get a copy of the DAL object.
+$data = new Data();
+
+$TitlErr = null;
+$DescErr = null;
+$StarErr = null;
+$EndTErr = null;
+$OrgaErr = null;
+$LocaErr = null;
+$ContErr = null;
+////
+//// Setup END
+////
+
+// If submit has been clicked:
+if (isset($_POST["clicked_submit"])) {
+
+    ////
+    //// Validation Checking START.
+    ////
+    require "PHP_VALIDATION/validation.php";
+
+    // Grab our data from the form.
+    $Titl = $_POST["txtTitl"];
+    $Desc = $_POST["txaDesc"];
+    $Orga = $_POST["txtOrga"];
+    $Loca = $_POST["txtLoca"];
+    $Cont = $_POST["txtCont"];
+    $Venu = $_POST["selVenu"];
+	$Febk = $_POST["selFbform"];
+	//$CAdmi = $_POST["selCAdmi"];
+	
+	if ($userid=='1'){
+		$CAdmi = $_POST["selCAdmi"];
+	}
+	else if($userid != '1'){
+		$CAdmi = $userid;
+	}
+		
+	
+    // Grab our datetime date and convert it into a mySQL dateTime
+    $Star["year"] = $_POST["selStarYear"];
+    $Star["month"] = $_POST["selStarMonth"];
+    $Star["day"] = $_POST["selStarDay"];
+    $Star["hour"] = $_POST["selStarHour"];
+    $Star["minute"] = $_POST["selStarMinute"];
+    $Star["second"] = "00";
+    //$Star["string"] = dtConvertToString($Star);
+	$StartDate = $Star["string"] = dtConvertToString($Star);
+
+    $EndT["year"] = $_POST["selEndTYear"];
+    $EndT["month"] = $_POST["selEndTMonth"];
+    $EndT["day"] = $_POST["selEndTDay"];
+    $EndT["hour"] = $_POST["selEndTHour"];
+    $EndT["minute"] = $_POST["selEndTMinute"];
+    $EndT["second"] = "00";
+    //$EndT["string"] = dtConvertToString($EndT);
+	$EndDate = $EndT["string"] = dtConvertToString($EndT);
+
+    // A bool flag that determines validation success.
+    $validated = true;
+
+    // nv() is a small shorthand function that switches the validation flag
+    // to false.  It then returns the current validation error from our validation lib.
+    function nv()
+    {
+        global $validated;
+        $validated = false;
+        return vGetErr();
+    }
+
+
+    // Validate Title:
+    if (vIsBlank($Titl) || !vMaxChars($Titl, 100)) // not blank, max 100 chars.
+        $TitlErr = nv();
+
+    // Validate Description:
+    if (vIsBlank($Desc) || !vMaxChars($Desc, 400)) // not blank, max 400 chars.
+        $DescErr = nv();
+
+    // Validate Start date:
+    if (vIsBlank($Star["string"]) || !vIsDate($Star)) // not blank, is sql datetime.
+        $StarErr = nv();
+
+    // Validate End date:
+    if (vIsBlank($EndT["string"]) || !vIsDate($EndT)|| vIsValidDate($StartDate,$EndDate)) // not blank, is sql datetime.
+        $EndTErr = nv();
+
+    // Validate Organisation:
+    if (vIsBlank($Orga) || !vMaxChars($Orga, 30)) // not blank, max 30 chars.
+        $OrgaErr = nv();
+
+    // Validate Location:
+    if (vIsBlank($Loca) || !vMaxChars($Loca, 70)) // not blank, max 70 chars.
+        $LocaErr = nv();
+
+    // Validate Cont:
+    if (vIsBlank($Cont) || !vMaxChars($Cont, 25)) // not blank, max 25 chars.
+        $ContErr = nv();
+
+    ////
+    //// Validation Checking END.
+    ////
+
+    // Once the data has been validated, let's insert the data.
+    if ($validated) {
+	
+        ////
+        //// Database Write START
+        ////
+     echo   $newData = array(
+                "Title" => $Titl,
+                "Description" => $Desc,
+                "Start_Time" => dtConvertToString($Star),
+                "End_Time" => dtConvertToString($EndT),
+                "Organiser" => $Orga,
+                "Location" => $Loca,
+                "Contact" => $Cont,
+                "Venue" => $Venu,
+				"Feedback" => $Febk,
+				"Conference_Admin_Id" => $CAdmi
+        );
+
+        //var_dump($newData);
+      	if ($data->conference->updateRow($_GET["id"], $newData))
+           header("Location: index.php?page=conference");
+
+        ////
+        //// Database Write END
+        ////
+    }
+} else {
+    // First page view:
+    // Lets load the data from the DB.
+
+    ////
+    //// Database Read START
+    ////
+
+    $row = $data->conference->getRow($_GET["id"]);
+
+    //$CoId = $row["ID"];
+    $Titl = $row["Title"];
+    $Desc = $row["Description"];
+    $Star = dtConvertToArray($row["Start_Time"]);
+    $EndT = dtConvertToArray($row["End_Time"]);
+    $Orga = $row["Organiser"];
+    $Loca = $row["Location"];
+    $Cont = $row["Contact"];
+    $Venu = $row["Venue"];
+	$Febk = $row["Feedback"];
+	$CAdmi = $row["Conference_Admin_Id"];
+}
+////
+//// Database Read END
+////
+
+////
+//// HTML Form START
+////
+?>
+
+<!-- Back button -->
+<div id='backButton' style='float: left;'>
+    <form action='index.php' method='get'>
+        <input type='hidden' name='page' value='conference'/>
+        <input type='submit' class='buttonStyle1' value='Go Back'/>
+    </form>
+</div>
+
+<!-- Heading -->
+<h1 style='text-align:center'>Edit a Conference</h1>
+
+<!-- Form START -->
+<form method='post' action='index.php?page=conference&action=edit&id=<?= $_GET["id"] ?>'>
+    <table class='std_form'>
+        <tr>
+            <td class='label'>Conference Title:</td>
+            <td><input type='text' class='textBoxStyle1' name='txtTitl' value='<?= $Titl ?>'/></td>
+            <td><span class='errorText'><?= $TitlErr ?></span></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>Description:</td>
+            <td><textarea name='txaDesc' class='textAreaStyle1' rows='4' value=><?= $Desc ?></textarea></td>
+            <td><span class='errorText'><?= $DescErr ?></span></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>Start Time:</td>
+            <td>
+                <table style="width:100%; margin:0px; border:0px; padding:0px;">
+                    <tr>
+                        <td>
+                            <select name='selStarDay' class='selectStyle1Dt'>
+                                <?= printOptionDays($Star["day"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <select name='selStarMonth' class='selectStyle1Dt'>
+                                <?= printOptionMonths($Star["month"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <select name='selStarYear' class='selectStyle1Dt'>
+                                <?= printOptionYears($Star["year"]) ?>
+                            </select>
+                        </td>
+                        <td style='padding-left:20px;'>
+                            <select name='selStarHour' class='selectStyle1Dt'>
+                                <?= printOptionHours($Star["hour"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <strong style='color:black'>:</strong>
+                        </td>
+                        <td>
+                            <select name='selStarMinute' class='selectStyle1Dt'>
+                                <?= printOptionMinutes($Star["minute"]) ?>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td><span class='errorText'><?= $StarErr ?></span></td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>End Time:</td>
+            <td>
+                <table style="width:100%; margin:0px; border:0px; padding:0px;">
+                    <tr>
+                        <td>
+                            <select name='selEndTDay' class='selectStyle1Dt'>
+                                <?= printOptionDays($EndT["day"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <select name='selEndTMonth' class='selectStyle1Dt'>
+                                <?= printOptionMonths($EndT["month"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <select name='selEndTYear' class='selectStyle1Dt'>
+                                <?= printOptionYears($EndT["year"]) ?>
+                            </select>
+                        </td>
+                        <td style='padding-left:20px;'>
+                            <select name='selEndTHour' class='selectStyle1Dt'>
+                                <?= printOptionHours($EndT["hour"]) ?>
+                            </select>
+                        </td>
+                        <td>
+                            <strong style='color:black'>:</strong>
+                        </td>
+                        <td>
+                            <select name='selEndTMinute' class='selectStyle1Dt'>
+                                <?= printOptionMinutes($EndT["minute"]) ?>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td><span class='errorText'><?= $EndTErr ?></span></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>Organiser:</td>
+            <td><input type='text' class='textBoxStyle1' name='txtOrga' value='<?= $Orga ?>'/></td>
+            <td><span class='errorText'><?= $OrgaErr ?></span></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>Location:</td>
+            <td><input type='text' class='textBoxStyle1' name='txtLoca' value='<?= $Loca ?>'/></td>
+            <td><span class='errorText'><?= $LocaErr ?></span></td>
+        </tr>
+        <tr>
+            <td colspan='2'>
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td class='label'>Contact:</td>
+            <td><input type='text' class='textBoxStyle1' name='txtCont' value='<?= $Cont ?>'/></td>
+            <td><span class='errorText'><?= $ContErr ?></span></td>
+        </tr>
+         <?php if($accesslevel == "1") { ?>
+        <tr>
+            <td colspan='2'>
+                <hr>
+            </td>
+        </tr>
+         <tr>
+                <td class='label'>Conference Administrator:</td>
+                <td>
+                    <select name='selCAdmi' class='selectStyle1'>
+                        <?PHP
+                        $data->user->printDropDownOptions($user, "User_name");
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <?php } ?>
+            <tr>
+                <td colspan='2'>
+                    <hr>
+                </td>
+            </tr>
+        <tr>
+            <td class='label'>Venue:</td>
+            <td>
+                <select name='selVenu' class='selectStyle1'>
+                    <?PHP
+                    $data->venue->printDropDownOptions($Venu, "Name");
+                    ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+                <td colspan="2">
+                    <hr>
+                </td>
+            </tr>
+        <tr>
+                <td class='label'>Feedback Form:</td>
+                <td>
+                    <select name='selFbform' class='selectStyle1'>
+                        <?PHP
+                        $data->feedback->printDropDownOptions(NULL, "Feedback_Title");
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <hr>
+                </td>
+            </tr>
+        <tr>
+            <td colspan='2'><span style='float: right;'>
+                	<br/>
+                    <input type='submit' value='Reset' name='clicked_reset' class="buttonStyle1"/>
+                    <input type='submit' value='Submit' name='clicked_submit' class='buttonStyle1'/>
+                    </span>
+            </td>
+        </tr>
+    </table>
+
+</form>
+<!-- Form End -->
+</div>
---
+++ Individual Documents/Rudhra/PROJ_NAL11358736_CONSTRUCTION_1/Programming/Completed Programs/HTML_INCLUDE/content.php
@@ -0,0 +1,279 @@
+<?PHP
+//
+// This script switches the correct content into the content div depending on the $page variable.
+//
+
+if ($page == "conference") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/conferences/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/conferences/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/conferences/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/conferences/delete.php';
+            break;
+        case "cloneView":
+            require 'PHP_PRES/conferences/cloneView.php';
+            break;
+        case "clone":
+            require 'PHP_PRES/conferences/clone.php';
+    }
+} else if ($page == "section") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/sections/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/sections/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/sections/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/sections/delete.php';
+            break;
+    }
+} else if ($page == "session") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/session/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/session/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/session/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/session/delete.php';
+            break;
+    }
+} else if ($page == "presenter") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/presenters/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/presenters/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/presenters/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/presenters/delete.php';
+            break;
+    }
+} else if ($page == "attendee_profile") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/attendee_profile/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/attendee_profile/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/attendee_profile/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/attendee_profile/delete.php';
+            break;
+    }
+} else if ($page == "user") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/users/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/users/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/users/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/users/delete.php';
+            break;
+    }
+} else if ($page == "feedback_question") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/feedback_question/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/feedback_question/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/feedback_question/add.php';
+            break;
+		case "delete":
+            require 'PHP_PRES/feedback_question/delete.php';
+            break;
+    }
+} else if ($page == "feedback_form") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/feedback_form/view.php';
+            break;
+        case "edit_s":
+            require 'PHP_PRES/feedback_form/edit_s.php';
+            break;
+        case "add_s":
+            require 'PHP_PRES/feedback_form/add_s.php';
+            break;
+        case "delete_s":
+            require 'PHP_PRES/feedback_form/delete_s.php';
+            break;
+    }
+} else if ($page == "polling_question") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/polling_question/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/polling_question/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/polling_question/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/polling_question/delete.php';
+            break;
+        case "result":
+            require 'PHP_PRES/polling_question/result.php';
+            break;
+        case "availability":
+            require 'PHP_PRES/polling_question/avail.php';
+            break;
+
+    }
+} else if ($page == "polling_result") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/polling_result/view.php';
+            break;
+        case "result":
+            require 'PHP_PRES/polling_result/result.php';
+            break;
+
+    }
+} else if ($page == "sponsor") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/sponsors/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/sponsors/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/sponsors/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/sponsors/delete.php';
+            break;
+    }
+} else if ($page == "review_question") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/review_question/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/review_question/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/review_question/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/review_question/delete.php';
+            break;
+        case "displaySelected":
+            require 'PHP_PRES/review_question/displaySelected.php';
+            break;
+    }
+} else if ($page == "map") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/map/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/map/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/map/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/map/delete.php';
+            break;
+        case "map_g":
+            require 'PHP_PRES/map/map_google.php';
+            break;
+        case "map_f":
+            require 'PHP_PRES/map/map_fixed.php';
+            break;
+    }
+} else if ($page == "home") {
+    require 'PHP_PRES/home.php';
+} else if ($page == "reset") {
+    switch ($action) {
+        case "reset":
+            require 'PHP_LOGIN/reset.php';
+            break;
+    }
+} else if ($page == "venue") {
+    switch ($action) {
+        case "view":
+            require 'PHP_PRES/venue/view.php';
+            break;
+        case "edit":
+            require 'PHP_PRES/venue/edit.php';
+            break;
+        case "add":
+            require 'PHP_PRES/venue/add.php';
+            break;
+        case "delete":
+            require 'PHP_PRES/venue/delete.php';
+            break;
+    }
+}else if ($page == "report") {
+    switch ($action) {
+        case "conference":
+            require 'PHP_PRES/conferences/conference_report.php';
+            break;
+        case "section":
+            require 'PHP_PRES/sections/section_report.php';
+            break;
+        case "session":
+            require 'PHP_PRES/session/session_report.php';
+            break;
+       case "question":
+            require 'PHP_PRES/feedback_question/question_report.php';
+            break;
+       case "polling":
+            require 'PHP_PRES/polling_question/polling_report.php';
+            break;
+       case "feedback":
+            require 'PHP_PRES/feedback_form/feedback_report.php';
+            break;
+    } 
+}
+// else if ($page == "map") {
+//     switch ($action) {
+//         case "view":
+//             require 'PHP_PRES/map/view.php';
+//             break;
+//         case "edit":
+//             require 'PHP_PRES/map/edit.php';
+//             break;
+//         case "add":
+//             require 'PHP_PRES/map/add.php';
+//             break;
+//         case "delete":
+//             require 'PHP_PRES/map/delete.php';
+//             break;
+//     }
+// }
+
+?>
