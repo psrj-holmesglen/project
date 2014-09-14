6b360deec74320bd3578d5f817bb73cfa18e190a
PaulGoncalves <p.goncalves@alerton.com.au>
2014-09-15 09:27:37 +1000

    Fix path for jspdf

--- www/root/PHP_PRES/feedback_form/feedback_report.php
+++ www/root/PHP_PRES/feedback_form/feedback_report.php
@@ -203,9 +203,9 @@ if ($CoId == "All" && $SesId == "All") {
 <img src="<?=IMG_PATH?>header.png" id="myimage" hidden="true" />
 <canvas width="590" height="50" id="mycanvas" style="display: none;"></canvas>
 
-<script type="text/javascript" src="<?=SCRIPT_PATH?>SCRIPTS_THIRD_PARTY/jspdf/from-html.js"></script>
-<script type="text/javascript" src="<?=SCRIPT_PATH?>SCRIPTS_THIRD_PARTY/jspdf/jspdf.debug.js"></script>
-<script type="text/javascript" src="<?=SCRIPT_PATH?>SCRIPTS_THIRD_PARTY/jspdf/jspdf.min.js"></script>
+<script type="text/javascript" src="<?=SCRIPT_PATH?>jspdf/from-html.js"></script>
+<script type="text/javascript" src="<?=SCRIPT_PATH?>jspdf/jspdf.debug.js"></script>
+<script type="text/javascript" src="<?=SCRIPT_PATH?>jspdf/jspdf.min.js"></script>
 <script>
 
 function savePdf() {
